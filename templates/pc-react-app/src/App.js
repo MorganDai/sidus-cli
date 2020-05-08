832b2f85926dab698c99d2b82d2e64d94e29c035
MorganDai <morgan.dai@sidus.link>
2020-05-08 12:20:14 +0800

    feat (pc-react-app) : add redux to projrect

--- templates/pc-react-app/src/App.js
+++ templates/pc-react-app/src/App.js
@@ -1,17 +1,29 @@
-import React from 'react';<% if (processor === 'Sass') { %>
-import './App.scss';<% } %><% if (processor === 'Less') { %>
-import './App.less';<% } %><% if (processor === 'Stylus') { %>
-import './App.styl';<% } %><% if (!processor) { %>
-import './App.css';<% } %>
+import React from 'react';
+import { createStore } from 'redux';
+import counter from './reducers';
+<% if (processor === 'Sass') { %> import './App.scss';<% }
+%><% if (processor === 'Less') { %> import './App.less';<% }
+%><% if (processor === 'Stylus') { %> import './App.styl';<% }
+%><% if (!processor) { %> import './App.css';<% } %>
 
-class App extends React.Component {
-	render() {
-		return (
-			<div className="sidus-container">
-				<h2>Welcome to Sidus!</h2>
-			</div>
-		)
-	}
+const store = createStore(counter);
+
+const App = () => {
+
+	const [val, setVal] = React.useState(store.getState());
+	const reGetVal = () => {
+		setVal(store.getState());
+	};
+
+	return (
+		<div className="sidus-container">
+			<h2>Welcome to Sidus!</h2>
+			<hr />
+			<h3>{val}</h3>
+			<button onClick={() => {store.dispatch({ type: 'INCREMENT' }); reGetVal()}}>点击增加</button>
+			<button onClick={() => {store.dispatch({ type: 'DECREMENT' }); reGetVal()}}>点击增加</button>
+		</div>
+	)
 }
 
 export default App;
